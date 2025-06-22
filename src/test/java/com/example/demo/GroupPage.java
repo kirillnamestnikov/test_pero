d48b5ff680ddef2e0066fb96398eb7215019f5a0
kirillnamestnikov <kir.namestnikov@gmail.com>
2025-06-22 20:52:04 +0300

    new Group Test

--- src/test/java/com/example/demo/GroupPage.java
+++ src/test/java/com/example/demo/GroupPage.java
@@ -7,10 +7,22 @@ import com.codeborne.selenide.SelenideElement;
 // page_url = https://vk.com/club225299895
 public class GroupPage {
     private SelenideElement name = $x("//h1[@class='page_name']");
+    private SelenideElement productPrice = $x("//div[contains(@class, 'EcommPreviewProductCardProductPrice__main')]");
 
     public GroupPage checkName(){
         name.shouldBe(visible.because("На странице с сообществом должно отображаться его имя")
         ).shouldHave(text("Test public for test"));
         return this;
+    }
+
+    public GroupPage checkPrice(){
+        productPrice.shouldBe(visible.because("На странице с сообществом должен быть товар с ценой")
+        ).shouldHave(text("300"));
+        return this;
+    }
+
+    public int getPriceCount(){
+        String text = productPrice.getText();
+        return Integer.parseInt(text.replaceAll("[^0-9]", ""));
     }
 }
--- src/test/java/com/example/demo/GroupPageTest.java
+++ src/test/java/com/example/demo/GroupPageTest.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.chrome.ChromeOptions;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -20,8 +23,15 @@ public class GroupPageTest {
         open("https://vk.com/club225299895");
     }
 
-    @Test
-    public void testInfo(){
+    @ParameterizedTest(name = "Проверка на то, что цена положительная")
+    @ValueSource(ints = {0, 100, 200})
+    public void testInfo(int price){
         groupPage.checkName();
+        groupPage.checkPrice();
+        int actualPrice = groupPage.getPriceCount();
+        assertTrue(actualPrice > price, String
+                .format("Цена товара должна быть больше %d",
+                        price)
+        );
     }
 }
